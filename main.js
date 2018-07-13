50a49492a57472b0be906517ca28b3a9c8b66e0e
Jissaura Taveras <tavej252@newschool.edu>
2018-07-12 22:08:36 -0400

    Complete Website re-design

---
+++ main.js
@@ -0,0 +1,58 @@
+(function(){
+    // Alternate Fixed & Scroll Backgrounds - by CodyHouse.co
+	// toggle main navigation on mobile
+	var mainNav = document.getElementsByClassName('js-main-nav')[0];
+	if(mainNav) {
+		mainNav.addEventListener('click', function(event){
+			if( hasClass(event.target, 'js-main-nav') ){
+				var navList = mainNav.getElementsByTagName('ul')[0];
+				toggleClass(navList, 'cd-main-nav__list--is-visible', !hasClass(navList, 'cd-main-nav__list--is-visible'));
+			} 
+		});
+	}
+	
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+	function toggleClass(el, className, bool) {
+		if(bool) addClass(el, className);
+		else removeClass(el, className);
+	}
+})();
+
+  AOS.init({
+        easing: 'ease-in-out-sine'
+      });
+
+var iconRotate = $('.asterisk img');
+if (iconRotate.length != 0) {
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop(),
+		maxScroll = $(document).height()-$(window).height();
+		iconRotate.css({transform: 'rotate(-' + (360 * scroll/maxScroll) + 'deg)'});
+	});
+}
+
+$(".tiptext").mouseover(function() {
+    $(this).children(".description").show();
+}).mouseout(function() {
+    $(this).children(".description").hide();
+});
+
